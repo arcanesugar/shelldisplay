dcea991b932bde0f1dd1f115453c12c78254e840
arcanesugar <thorntonjamesb@gmail.com>
2025-05-21 19:04:29 +0000

    removed shellpng.c, will be re-added later

--- main.c
+++ main.c
@@ -3,19 +3,7 @@
 #include "shelldisplay.h"
 
 int main(){
-  setColorMode(CM_TRUECOLOR);
-  Texture t = createTexture(2, 2);
-  setPixel(t, 0, 0, (Color){1,0,0});
-  setPixel(t, 1, 0, (Color){0,1,0});
-  setPixel(t, 0, 1, (Color){0,0,1});
-  setPixel(t, 1, 1, (Color){0,0,0});
-  for(int i = 0; i<t.width*t.height; i++){
-    //t.pixels[i] = invertColor(t.pixels[i]);
-    t.pixels[i] = multiplyColors(t.pixels[i], (Color){1,1,0});
-  }
-  Texture screen = resampleTexture(t, 90, 90, SAMPLE_BILINEAR);
-  setStyle(S_BLINK|S_BOLD|S_UNDERLINE|S_ITALIC|S_STRIKETHROUGH);
-  printf("TEXTTEXTTEXTTEXTTEXT\n");
-  printTexture(screen, PIX_UPPER_HALF_BLOCK);
+  setStyle(S_BOLD|S_ITALIC);
+  printf("Example program\n");
   return 0;
 };
